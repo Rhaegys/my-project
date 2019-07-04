7746365c8674013611d11443df110e43325eed9b
Rhaegys <Rhaegys@gmail.com>
2019-07-05 00:47:55 +0300

    refactored a lil bit2
    pu2

--- src/Controller/ProcessDataController.php
+++ src/Controller/ProcessDataController.php
@@ -2,14 +2,9 @@
 
 namespace App\Controller;
 
-<<<<<<< Updated upstream
-use App\DataProcessing\Data; 
-use App\DataProcessing\DataProcessing\AV; 
-=======
 //use App\DataProcessing\Data;
 use App\DataProcessing\DataProcessing;
 
->>>>>>> Stashed changes
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\NativeHttpClient;
@@ -17,8 +12,6 @@ use Symfony\Component\HttpClient\NativeHttpClient;
 class ProcessDataController extends AbstractController
 {
     /**
-<<<<<<< Updated upstream
-=======
      * @var DataRetrieval
      */
     private $dataRetrieval;
@@ -34,19 +27,12 @@ class ProcessDataController extends AbstractController
     }
 
     /**
->>>>>>> Stashed changes
      * @Route("/process/data", name="process_data")
      */
     public function index()
     {
-<<<<<<< Updated upstream
-        $pup = new Data(new AV);
-        $d1=$pup->getData();
-        $contents=$pup->refactorData($d1);
-=======
         $dataRaw = $this->dataRetrieval->getData();
         $data=$this->dataRefactoring->refactorData($dataRaw);
->>>>>>> Stashed changes
 
 
         return $this->render('process_data/index.html.twig', [
